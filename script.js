5e3fcf7b2fc233968d764e934b2b6a99ddcfcd2d
armaanhossainovi102030-wq <armaanhossainovi102030@gmail.com>
2026-03-07 02:11:53 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function login(){
+    let userName= document.getElementById("username").value
+let userPassword= document.getElementById("password").value
+
+if (userName === "admin" && userPassword==="admin123"){
+    window.location.href = "main.html"
+}
+else{
+    console.log("not ok")
+}
+}
